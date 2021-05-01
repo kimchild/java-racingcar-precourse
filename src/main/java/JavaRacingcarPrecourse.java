812a8745e888a16f9424828200c3e4d8816de7d7
kimchild <allcreation@nate.com>
2021-05-01 18:02:28 +0900

    feat(UserInterfaceManager.java): 우승자를 알려주는 기능개발
    사용자에게 최종 우승자를 보여주기위해
    우승자를 알려주는 기능
    
    사용자에게 우승자를 알려주는 기능

--- src/main/java/JavaRacingcarPrecourse.java
+++ src/main/java/JavaRacingcarPrecourse.java
@@ -7,6 +7,8 @@ public class JavaRacingcarPrecourse {
 	public static void main(String[] args) {
 		try (Scanner scan = new Scanner(System.in)) {
 			new UserInterfaceManager().start(scan);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/main/java/ui/UserInterfaceManager.java
+++ src/main/java/ui/UserInterfaceManager.java
@@ -1,7 +1,9 @@
 package ui;
 
+import java.util.List;
 import java.util.Scanner;
 
+import domain.Car;
 import domain.GameManager;
 
 public class UserInterfaceManager {
@@ -13,9 +15,24 @@ public class UserInterfaceManager {
 		System.out.println("경주할 자동차 이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
-	public void start(Scanner scan) {
+	public void start(Scanner scan) throws InterruptedException {
 		this.reset();
 		final int count = userInput(scan);
+		userOutput(count);
+	}
+
+	private void userOutput(int count) throws InterruptedException {
+		int index = 0;
+		do {
+			gameManager.action();
+			System.out.println("실행 결과");
+
+			this.movePrint(gameManager.getCarList());
+			index++;
+			Thread.sleep(2000);
+		} while (count > index);
+
+		System.out.println(gameManager.getWinners() + "가 최종 우승했습니다.");
 	}
 
 	private int userInput(Scanner scan) {
@@ -25,5 +42,24 @@ public class UserInterfaceManager {
 		gameManager.setCount(count);
 		gameManager.setUp();
 		return count;
+	}
+
+	private void movePrint(List<Car> carList) {
+		StringBuilder sb;
+		for (Car car : carList) {
+			sb = new StringBuilder();
+			sb.append(car.getName());
+			sb.append(" : ");
+			sb.append(this.getMovement(car.getMove()));
+			System.out.println(sb);
+		}
+	}
+
+	private String getMovement(int move) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < move; i++) {
+			sb.append("-");
+		}
+		return sb.toString();
 	}
 }
