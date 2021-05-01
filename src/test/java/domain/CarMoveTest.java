c22a1bbbe9439759267f625baf22f1ac85da05ab
kimchild <allcreation@nate.com>
2021-05-01 15:26:09 +0900

    test(CarMoveTest.java): 테스트검증 컨벤션 수정
    테스트코드 컨벤션 수정

--- src/test/java/domain/CarMoveTest.java
+++ src/test/java/domain/CarMoveTest.java
@@ -2,17 +2,9 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarMoveTest {
-	private Car car;
-
-	@BeforeEach
-	void setUp() {
-		this.car = new Car("pobi");
-	}
-
 	@Test
 		// @checkstyle:off
 	void 자동차_전진유무판단_이동() {
@@ -21,16 +13,12 @@ public class CarMoveTest {
 
 		for (int i = 0; i < 100; i++) {
 			/* when */
+			Car car = new Car("pobi");
 			car.random();
-			boolean isMove = car.getRandomNumber() >= Car.RULE_VALUE;
 			car.move();
 
 			/* then */
-			if (isMove) {
-				assertThat(car.getMove() > 0).isTrue();
-				return;
-			}
-			assertThat(car.getMove() > 0).isFalse();
+			assertThat(car.getMove() > 0).isEqualTo(car.isRule());
 		}
 	}
 
